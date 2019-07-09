d915e59eccea7f029d35fa3d77a057f526f94cc8
Stephen Klosky <sklosky@gmail.com>
2019-07-09 15:02:47 -0400

    Update getData.ps1

--- getData.ps1
+++ getData.ps1
@@ -1,7 +1,8 @@
 # a script to pull down gdelt data
 # see gdeltproject.org for details
+$masterFile = "masterfilelist.txt"
 $lines = 0
-foreach($line in Get-Content .\masterfilelist.txt){
+foreach($line in Get-Content ".\" & $masterFile){
   $lines = $lines + 1
 }
 echo $lines
