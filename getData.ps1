3c5b665c57e7696277a540d48dd45a87ff24b06c
Stephen Klosky <sklosky@gmail.com>
2019-07-09 15:10:33 -0400

    Update getData.ps1

--- getData.ps1
+++ getData.ps1
@@ -1,8 +1,12 @@
 # a script to pull down gdelt data
 # see gdeltproject.org for details
+Import-Module BitsTransfer
 $masterFile = "masterfilelist.txt"
+$webMasterFile = "http://data.gdeltproject.org/gdeltv2/masterfilelist.txt"
+$output = ".\$masterFile"
+Start-BitsTransfer -Source $webMasterFile -Destination $output
 $lines = 0
-foreach($line in Get-Content ".\" & $masterFile){
+foreach($line in Get-Content ".\$masterFile"){
   $lines = $lines + 1
 }
 echo $lines
