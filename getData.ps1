e16bf9fd3e79b921036a7338f29c20a0fe9dcb3d
Stephen Klosky <sklosky@gmail.com>
2019-07-09 15:24:04 -0400

    Update getData.ps1

--- getData.ps1
+++ getData.ps1
@@ -4,9 +4,17 @@ Import-Module BitsTransfer
 $masterFile = "masterfilelist.txt"
 $webMasterFile = "http://data.gdeltproject.org/gdeltv2/masterfilelist.txt"
 $output = ".\$masterFile"
+$startAt = 454650
+cd ~\Downloads
 Start-BitsTransfer -Source $webMasterFile -Destination $output
 $lines = 0
 foreach($line in Get-Content ".\$masterFile"){
   $lines = $lines + 1
+  if ($lines -gt $startAt){
+    $lineArray = $line.split(" ")
+    $url = $lineArray[2]
+    $output = ".\$lineArray[0].zip"
+    Start-BitsTransfer -Source $url -Destination $output
+  }
 }
 echo $lines
